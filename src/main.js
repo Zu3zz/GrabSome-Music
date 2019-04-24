f3d4d43c3a93dd2a1dd318c2aee246e5a9f19784
Zu3zz <zthzththth@163.com>
2019-04-24 15:55:04 +0800

    finish recommend

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 new Vue({
   router,
